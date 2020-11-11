4f851d171257f546f95600bb05f059233b53066d
Abdo KD <abdokardidi44@gmail.com>
2020-11-11 01:39:41 +0100

    final

--- src/redux/baseUrls.js
+++ src/redux/baseUrls.js
@@ -1,3 +1,3 @@
-export const service_utilisateur_baseUrl = 'https://elearning-users.herokuapp.com/';
-export const service_cours_baseUrl = 'https://elearning-cours.herokuapp.com/';
-export const service_image_baseUrl = 'https://elearning-images.herokuapp.com/';
+export const service_utilisateur_baseUrl = 'http://localhost:8081/';
+export const service_cours_baseUrl = 'http://localhost:8083/';
+export const service_image_baseUrl = 'http://localhost:8082/';
